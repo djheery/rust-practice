6f08e80bb56c8bb688f750e8eed94fda8bc31633
Daniel Heery <danieljheery@gmail.com>
2023-05-04 18:41:04 +0100

    Screen Layout Practice

--- screen_layout/src/main.rs
+++ screen_layout/src/main.rs
@@ -3,49 +3,50 @@ mod renderer;
 
 use sdl2::event::Event; 
 use sdl2::keyboard::Keycode; 
-use std::time::Duration; 
 use crate::renderer::Renderer;
 pub use sdl2::pixels::Color;
 pub use sdl2::render::WindowCanvas;
 pub use sdl2::rect::Rect;
 pub use sdl2::video::Window;
+pub use std::time::Duration;
 
-pub const X_SIZE: i32 = 40; 
-pub const Y_SIZE: i32 = 40; 
-pub const PIX: i32 = 20;
+pub const SCREEN_WIDTH: u32 = 800; 
+pub const SCREEN_HEIGHT: u32 = 600; 
 
-fn main() {
-  let sdl_context = sdl2::init().unwrap();
-  let video_subsystem = sdl_context.video().unwrap();
+fn main() -> Result<(), String> {
+  let sdl_context = sdl2::init()?; 
+  let video_subsystem = sdl_context.video()?; 
+  let window = video_subsystem
+               .window("Layout Test", SCREEN_WIDTH, SCREEN_HEIGHT)
+               .position_centered()
+               .opengl()
+               .build()
+               .map_err(|e| e.to_string())?; 
+               
 
-  let window = video_subsystem.window("rust-sdl2 demo", 600, 600)
-      .position_centered()
-      .build()
-      .unwrap();
-
-  let mut canvas = window.into_canvas().build().unwrap();
-
-  canvas.set_draw_color(Color::RGB(0, 255, 255));
-  canvas.clear();
-  canvas.present();
-  let mut event_pump = sdl_context.event_pump().unwrap();
-  let mut i = 0;
+  let mut renderer = Renderer::new(window)?;
+  let mut context = sdl_context.event_pump()?;
+  let mut frame_counter = 0;
   'running: loop {
-      i = (i + 1) % 255;
-      canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-      canvas.clear();
-      for event in event_pump.poll_iter() {
-          match event {
-              Event::Quit {..} |
-              Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                  break 'running
-              },
-              _ => {}
+    for event in context.poll_iter() {
+      match event {
+        Event::Quit { .. } => break 'running,
+        Event::KeyDown { keycode: Some(keycode), .. } => {
+          match keycode {
+            Keycode::Escape => break 'running,
+            _ => {}
           }
+        } 
+        _ => {}
       }
-      // The rest of the game loop goes here...
-
-      canvas.present();
-      ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+    frame_counter += 1; 
+    if frame_counter & 145 == 0 {
+      frame_counter = 0; 
+    }
+    renderer.draw()?;
+    ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 40));
   }
+
+  Ok(())
 }
--- screen_layout/src/renderer/mod.rs
+++ screen_layout/src/renderer/mod.rs
@@ -1,3 +1,38 @@
 pub use sdl2::render::WindowCanvas;
+use sdl2::video::Window; 
+use crate::Color; 
+use crate::Rect; 
+
 
 pub struct Renderer { canvas: WindowCanvas }
+
+impl Renderer {
+  pub fn new(window: Window) -> Result<Renderer, String> {
+    let canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+    Ok(Renderer { canvas })
+  }
+
+  pub fn draw(&mut self) -> Result<(), String> {
+    self.draw_background();
+    self.canvas.present();
+    Ok(())
+  }
+
+  pub fn draw_background(&mut self) {
+    let color = Color::RGB(28, 63, 98);
+    self.canvas.set_draw_color(color);
+    self.canvas.clear();
+    let row_num = 600 / 6;
+    let col_num = 800 / 8; 
+    println!("r{row_num} c{col_num}");
+    for row in 0..row_num {
+      for col in 0..col_num {
+        println!("{col}");
+        let x: i32 = ((row_num * row) + 10).try_into().unwrap(); 
+        let y: i32 = ((col_num * col) + 10).try_into().unwrap();
+        self.canvas.set_draw_color(Color::RGB(255, 255, 255));
+        self.canvas.fill_rect(Rect::new(x, y, 100 - 10, 100 - 10));
+      }
+    }
+  }
+}
